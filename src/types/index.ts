4be9b59fc53abcb0f6a3f2239f8da84ba8862a05
akhil-haridas <akhil.k@dignizant.com>
2024-06-14 18:08:41 +0530

    created type for the email

--- src/types/index.ts
+++ src/types/index.ts
@@ -4,8 +4,10 @@ type CommonFields = {
 }
 
 export const CompaignStatusArray  = ['Draft', 'Sent', 'Archived'];
+export const EmailStatusArray  = ['Draft', 'Completed'];
 
 type CompaignStatus = typeof CompaignStatusArray[number]
+type EmailStatus = typeof EmailStatusArray[number]
 
 export type Compaign = {
     name: string;
@@ -16,8 +18,15 @@ export type Compaign = {
     status?: CompaignStatus | undefined;
 } & CommonFields;
 
-export type Email = {} & CommonFields;
+export type Email = {
+    title: string;
+    content: string;
+    status: EmailStatus;
+} & CommonFields;
 
-export type Subscriber = {} & CommonFields;
+export type Subscriber = {
+    email: string;
+    user_id: string;
+} & CommonFields;
 
 export type User = {} & CommonFields;
