9dd08e6a894015dcef189a0078d2a0634263f9d6
akhil-haridas <akhil.k@dignizant.com>
2024-06-14 18:16:01 +0530

    function for validate the email

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,1 +1,9 @@
 // all the basic functions for the app
+
+export const isValidEmail = (email: string | undefined | null): boolean => {
+    if (!email) return false;
+    const regex = new RegExp(
+        /^(([*<>()\[\]\\.,;:\s@"]+(\.[*<>()\[\]\\.,;:\s@"]+)*)|(".+"))@ ((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}]) | (([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+    return email ? regex.test(email) : false;
+};
