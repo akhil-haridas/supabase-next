e4ff28585f8e5bc859f870e7d74511cf30e56cdf
akhil-haridas <akhil.k@dignizant.com>
2024-06-14 18:18:23 +0530

    removed repeated condition

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,6 @@
 // all the basic functions for the app
 
 export const isValidEmail = (email: string | undefined | null): boolean => {
-    if (!email) return false;
     const regex = new RegExp(
         /^(([*<>()\[\]\\.,;:\s@"]+(\.[*<>()\[\]\\.,;:\s@"]+)*)|(".+"))@ ((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}]) | (([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     );
